a71524d28217af8946914bbbb07a1ffebf2fc524
Matt Odille <matthew.odille@arrivehealth.com>
2024-02-07 13:56:37 -0500

    Fix commands to install tgenv
    The package from homebrew is out-of-date and failed to install 0.44.5
    with:
    
    > tgenv: tgenv-install: [ERROR] No versions matching '0.44.5' found in remote

--- scripts/opt-in/terraform.sh
+++ scripts/opt-in/terraform.sh
@@ -1,9 +1,13 @@
 echo
 echo "Installing Terraform tooling"
 
-brew install tfenv tgenv
+brew install tfenv
 tfenv install "${DEFAULT_TF_VERSION}"
-tgenv install "${DEFAULT_TG_VERSION}"
+
+git clone --depth 1 --branch main https://github.com/tgenv/tgenv.git ~/.tgenv
+echo 'export PATH="$HOME/.tgenv/bin:$PATH"' >> ~/.zshrc
+export PATH="$HOME/.tgenv/bin:$PATH"
+~/.tgenv/bin/tgenv install "$DEFAULT_TG_VERSION"
 
 # shell completion
 terraform -install-autocomplete || true
