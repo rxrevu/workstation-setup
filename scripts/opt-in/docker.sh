3588fbc3973c04d7cffcb22a28bff51db48b5a8c
Matt Odille <matthew.odille@arrivehealth.com>
2024-02-07 12:51:17 -0500

    Fix docker completion script
    Taken from the latest docs https://docs.docker.com/desktop/faqs/macfaqs/#how-do-i-install-shell-completion

--- scripts/opt-in/docker.sh
+++ scripts/opt-in/docker.sh
@@ -7,12 +7,15 @@ brew install docker-compose
 
 echo "To get docker command-line tools, run the docker application"
 
-# Docker Bash Completion
-# Reference https://docs.docker.com/docker-for-mac/
-pushd /usr/local/etc/bash_completion.d
-  ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
-  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
-  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
-popd
+# Docker completion
+# https://docs.docker.com/desktop/faqs/macfaqs/#how-do-i-install-shell-completion
+etc=/Applications/Docker.app/Contents/Resources/etc
+# Bash
+ln -s $etc/docker.bash-completion "$(brew --prefix)/etc/bash_completion.d/docker"
+ln -s $etc/docker-compose.bash-completion "$(brew --prefix)/etc/bash_completion.d/docker-compose"
+echo '[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion' >> ~/.bash_profile
+# Zsh
+ln -s $etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
+ln -s $etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
 
 set -e
