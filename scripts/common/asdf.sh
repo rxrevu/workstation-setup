f39ba63cd471eefb57e2a34c08266bbe4d86d269
Matt Odille <matthew.odille@arrivehealth.com>
2024-02-07 15:03:42 -0500

    Use latest steps for installing asdf
    Taken from https://asdf-vm.com/guide/getting-started.html

--- scripts/common/asdf.sh
+++ scripts/common/asdf.sh
@@ -1,11 +1,10 @@
-  if [[ ! -d ~/.asdf ]]; then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+if [[ ! -d ~/.asdf ]]; then
+  brew install coreutils curl git
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 
-    echo '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-    echo '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
-    . "$HOME/.asdf/asdf.sh"
-    brew install coreutils
-    brew install gpg
-  else
-    printf "asdf already installed skipping\n"
-  fi
+  echo '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+  echo '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+  . "$HOME/.asdf/asdf.sh"
+else
+  printf "asdf already installed. skipping\n"
+fi
