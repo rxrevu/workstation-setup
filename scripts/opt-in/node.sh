28d8905b2fb7d110875d46c0d4c7c09018d986f2
Andrew Courter <andrew.courter@arrivehealth.com>
2024-03-04 17:39:31 -0600

    Create completions file for fnm

--- scripts/opt-in/node.sh
+++ scripts/opt-in/node.sh
@@ -2,7 +2,8 @@ echo
 echo "Installing most recent version of NodeJS"
 
 brew install fnm
-fnm completions --shell zsh
+mkdir ~/.oh-my-zsh/completions
+fnm completions --shell zsh > ~/.oh-my-zsh/completions/_fnm
 echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc
 fnm install --lts
 
