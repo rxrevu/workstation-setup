9760d71bc0a76f1491a0567aa0d797edce95c92e
Andrew Courter <andrew.courter@arrivehealth.com>
2024-06-20 08:36:38 -0500

    Revert "Update install scripts to use fnm & frum"
    This reverts commit 9516abec5a3492faab6941f2bcee8d555bcbd1b7.

--- _init.sh
+++ _init.sh
@@ -1,7 +1,8 @@
 # Shared setup initialization
 
 export WORKSTATION_SETUP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export DEFAULT_RUBY_VERSION=3.3.0
+export DEFAULT_RUBY_VERSION=3.2.2
 export DEFAULT_PYTHON_VERSION=3.11.7
+export DEFAULT_NODEJS_VERSION=20.10.0
 export DEFAULT_TF_VERSION=1.7.1
 export DEFAULT_TG_VERSION=0.54.22
